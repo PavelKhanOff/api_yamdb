c54b106cd4071f17229f24f5e215b9e4c61a7588
Yulia Bannikova <74221879+AltBansha@users.noreply.github.com>
2021-03-27 13:05:04 +0500

    Develop yulia 1 (#8)
    * Dudi (#6)
    
    * firt try models
    
    * views review and comment
    
    Co-authored-by: PavelKhanOff <71562711+PavelKhanOff@users.noreply.github.com>
    
    * Changes
    
    * One more try
    
    Co-authored-by: Dudi001 <74237735+Dudi001@users.noreply.github.com>
    Co-authored-by: PavelKhanOff <71562711+PavelKhanOff@users.noreply.github.com>

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'yamdb',
     'rest_framework',
+    'django_filters',
 
 ]
 
@@ -103,6 +104,9 @@ EMAIL_USE_SSL = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
--- yamdb/urls.py
+++ yamdb/urls.py
@@ -1,11 +1,34 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
 from rest_framework_simplejwt.serializers import TokenObtainSerializer
-from .views import SendMessage, SendToken
+
+from .views import (
+    SendMessage, SendToken,
+    CategoryViewSet, GenreViewSet,
+    TitleViewSet, CommentViewSet, ReviewViewSet
+)
+
+router = DefaultRouter()
+
+router.register('titles', TitleViewSet, basename='titles')
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('genres', GenreViewSet, basename='genres')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
     path('v1/auth/email/', SendMessage, name='message_sending_view'),
     path('v1/auth/token/', SendToken, name='token_obtain_pair'),
-      ]
+]

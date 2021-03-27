9c600b56d6e0d32ceea255856d049efe6aaf7caa
Pavel Khan <Pavelkhanoff@gmail.com>
2021-03-27 18:52:34 +0600

    alldonehopeitsend

--- yamdb/filters.py
+++ yamdb/filters.py
@@ -1,21 +1,12 @@
-from django_filters import filters
+from django_filters import rest_framework as filters
 
 from .models import Title
 
 
 class TitleFilter(filters.FilterSet):
-    name = filters.CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    )
-    category = filters.CharFilter(
-        field_name='category__slug',
-        lookup_expr='contains'
-    )
-    genre = filters.CharFilter(
-        field_name='genre__slug',
-        lookup_expr='contains'
-    )
+    category = filters.CharFilter(field_name='category__slug')
+    genre = filters.CharFilter(field_name='genre__slug')
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
         model = Title
--- yamdb/migrations/0010_auto_20210327_1337.py
+++ yamdb/migrations/0010_auto_20210327_1337.py
@@ -6,7 +6,6 @@ import django.utils.timezone
 from django.db import migrations, models
 
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
